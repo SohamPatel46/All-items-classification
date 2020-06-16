1ba4a2a8853da7f7f101af364211c1fc560e6d6d
Soham Patel <soham.patel105876@marwadiuniversity.ac.in>
2020-06-16 11:57:55 +0530

    Keep these along dataset (ROOM ITEMS) folder.

---
+++ items_test.py
@@ -0,0 +1,94 @@
+#model = keras.models.load_model('/content/drive/My Drive/model.h5')
+import os
+from tensorflow import keras
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+import cv2 as cv
+from sklearn.neighbors import NearestNeighbors
+import sys 
+
+f=__file__
+f=f.replace('items_test.py', '')
+root_dir=f+'ROOM ITEMS'
+model = keras.models.load_model(f+'/model.h5')
+
+
+labels=["BEDSIDE CLOCK","BEDSIDE TABLE","BLANKET BAGS",'CLOTH BRUSH & SHOE HORN',
+        'CLOTH HANGER','COFFE & TEA PRESENTATION','DESK ORGANISERS',"Do Not Disturb Sign",
+        "GUEST ROOM BLOTTERS","GUEST ROOM COASTERS"]
+# predicting images 
+classes=[]
+query_path=sys.argv[1]
+#query_path = input("Enter test path : ")
+img = image.load_img(query_path, target_size=(150, 150))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+images = np.vstack([x])
+classes = model.predict(images, batch_size=10)
+
+for x in range(10):
+  if classes[0][x]>0.5:
+    cls=labels[x]
+    print("Classified as ",cls)
+
+#traning
+print("Training Started")
+img_dir = os.path.join(root_dir+'/'+cls)
+img=os.listdir(img_dir)
+img_paths=[]
+for i in img:
+  s=img_dir+'/'+i
+  img_paths.append(s)
+
+model1 = VGG16(weights='imagenet', include_top=False)
+
+img_vector_features = []
+for img_path in img_paths:
+  img = image.load_img(img_path, target_size=(150,150))
+  img_data = image.img_to_array(img)
+  img_data = np.expand_dims(img_data, axis=0)
+  img_data = preprocess_input(img_data)
+
+  vgg16_feature = model1.predict(img_data)
+  vgg16_feature = np.array(vgg16_feature)
+  vgg16_feature = vgg16_feature.flatten()
+  img_vector_features.append(vgg16_feature)
+
+#testing
+print("Testing Started")
+img = image.load_img(query_path, target_size=(150,150))
+img_data = image.img_to_array(img)
+img_data = np.expand_dims(img_data, axis=0)
+img_data = preprocess_input(img_data)
+
+vgg16_feature = model1.predict(img_data)
+vgg16_feature = np.array(vgg16_feature)
+query_feature = vgg16_feature.flatten()
+
+# Numbers of similar images that we want to show
+if len(img_paths)<10:
+  N_QUERY_RESULT = len(img_paths)
+else: 
+  N_QUERY_RESULT = 9
+nbrs = NearestNeighbors(n_neighbors=N_QUERY_RESULT, metric="cosine").fit(img_vector_features)
+
+distances, indices = nbrs.kneighbors([query_feature])
+similar_image_indices = indices.reshape(-1)
+
+print("Got {} similar images.".format(N_QUERY_RESULT))
+
+#a = fig.add_subplot(5, 5, 1)
+img = cv.imread(query_path) 
+for i in range(N_QUERY_RESULT):
+  a = fig.add_subplot(5, 5, i+1)
+  img = cv.imread(img_paths[similar_image_indices[i]]) 
+  imgplot = cv.imshow(img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
---
+++ items_train.py
@@ -0,0 +1,31 @@
+import os
+from tensorflow import keras
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing.image import ImageDataGenerator
+
+f=__file__
+f=f.replace('items_train.py', '')
+root=f+'ROOM ITEMS'
+root_dir = os.path.join(root)
+
+TRAINING_DIR = root_dir
+training_datagen = ImageDataGenerator(rescale = 1./255)
+train_generator = training_datagen.flow_from_directory(TRAINING_DIR,	target_size=(150,150),	
+                                                       class_mode='categorical',batch_size=1)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(loss = 'categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+history = model.fit(train_generator, epochs=10,  verbose = 1)
+model.save(f+"/model.h5")
