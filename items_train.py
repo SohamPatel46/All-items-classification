91e912a7f6e6b49e20246aae37798525112c7a2d
Soham Patel <soham.patel105876@marwadiuniversity.ac.in>
2020-06-16 13:40:24 +0530

    Update items_train.py

--- items_train.py
+++ items_train.py
@@ -6,13 +6,14 @@ from keras_preprocessing.image import ImageDataGenerator
 
 f=__file__
 f=f.replace('items_train.py', '')
+model_save=os.path.join(f)
 root=f+'ROOM ITEMS'
 root_dir = os.path.join(root)
 
 TRAINING_DIR = root_dir
 training_datagen = ImageDataGenerator(rescale = 1./255)
 train_generator = training_datagen.flow_from_directory(TRAINING_DIR,	target_size=(150,150),	
-                                                       class_mode='categorical',batch_size=1)
+                                                       class_mode='categorical',batch_size=2)
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
@@ -28,4 +29,4 @@ model = tf.keras.models.Sequential([
 
 model.compile(loss = 'categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 history = model.fit(train_generator, epochs=10,  verbose = 1)
-model.save(f+"/model.h5")
+model.save(model_save+"model.h5")
